cd590ee572b3d1f683751039fe74055eed3a3123
uxhawk <uxhawk@gmail.com>
2020-06-22 16:41:44 -0400

    added container fluid to app wrapper

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div>
       <StoreProvider>
         <Nav />
-          <div className="row">
-            <div className="col-md-8 offset-md-2 mt-4">
+          <div className="row container-fluid m-0">
+            <div className="col-md-10 offset-md-1 mt-4">
             <MarketOveriew />
             </div>
           </div>
