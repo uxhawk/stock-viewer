768373a27246e939ca1e77a9c32bbbf679ee85ce
uxhawk <uxhawk@gmail.com>
2020-06-23 20:59:04 -0400

    added a dollar sign to current search

--- src/components/CurrentSearch.js
+++ src/components/CurrentSearch.js
@@ -8,7 +8,7 @@ const CurrentSearch = (props) => {
             <p className="small p-0 m-0">{props.currentSearch.name}</p>
             </div>
             <div className="card-body">
-                <h5 className="mb-0 pb-0">{props.currentSearch.currentPrice}</h5>
+                <h5 className="mb-0 pb-0">${props.currentSearch.currentPrice}</h5>
                 <p className="small pt-0 mt-0">Updated At: {props.currentSearch.lastUpdate}</p>
                 <button className="btn btn-info" ><span className="fas fa-sync-alt pr-2" ></span>Refresh</button>
                 
